fb6e7a81715a67799b5460f83a8aa128c17f3748
SoumikiSaha <163008155+SoumikiSaha@users.noreply.github.com>
2025-12-09 01:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.21%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,7 +1,25 @@
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         # digit = 0
+#         # for d in digits:
+#         #     digit = digit * 10 + d
+#         # digit += 1
+#         # return [int(_f) for _f in str(digit)]
+#         if digits[-1] < 9:
+#             digits[-1] += 1
+        
+#         return digits
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        digit = 0
-        for d in digits:
-            digit = digit * 10 + d
-        digit += 1
-        return [int(_f) for _f in str(digit)]
+        i = len(digits) - 1
+
+        # Start from the last digit
+        while i >= 0:
+            if digits[i] < 10 - 1:   # If digit < 9
+                digits[i] += 1
+                return digits
+            digits[i] = 0           # If digit == 9, set to 0 and carry over
+            i -= 1
+
+        # If all digits were 9 (e.g., 999 → 000), add 1 at beginning
+        return [1] + digits
