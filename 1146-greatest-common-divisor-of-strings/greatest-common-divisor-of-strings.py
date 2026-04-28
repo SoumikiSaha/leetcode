7cb313a9e6d10bbc1cf63efd57220777d8f1cad4
SoumikiSaha <163008155+SoumikiSaha@users.noreply.github.com>
2026-04-29 00:33:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (71.72%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,14 +1,14 @@
 import re
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        def compute_gcd(a, b):
-            while b:
-                a, b = b, a % b
-            return a
-
         if str1 + str2 != str2 + str1:
             return ""
+        
+        def gcd(a, b):
+            while b:
+                a , b = b, a%b
+            return a
+        
+        gcd_len = gcd(len(str1), len(str2))
 
-        gcd_length = compute_gcd(len(str1), len(str2))
-
-        return str1[:gcd_length]
+        return str1[:gcd_len]
