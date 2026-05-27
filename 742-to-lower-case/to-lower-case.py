3240f3767d1d115ce646ba61c2ea66aee6d4e420
SoumikiSaha <163008155+SoumikiSaha@users.noreply.github.com>
2026-05-27 13:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (83.56%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        ans = ''
+        for i in s:
+            if i.isupper():
+                ans += i.lower()
+            else:
+                ans += i
+        return ans
